6249914091dd3dc7fcb30262fc186b087ead2808
Christopher Allan Webber <cwebber@dustycloud.org>
2013-10-11 19:15:58 -0500

    Kicking off emacslisten.
    There's nothing to see yet here really.  I took stuff from
    http://cmusphinx.sourceforge.net/wiki/gstreamer and that's about it ;)

---
+++ emacslisten.el
@@ -0,0 +1,20 @@
+;;; emacslisten.el --- control emacs with your voice!
+
+;; Copyright 2013 Christopher Allan Webber
+
+;; Emacslisten is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; Emacslisten is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with Emacslisten.  If not, see <http://www.gnu.org/licenses/>.
+
+;; This doesn't do anything yet.  I sure hope it will, though :)
+
+
